7f65f21bd9e34bdffbe31466ebe62ce720b4903c
talhatelli <ahmettalhatelli1453@gmail.com>
2022-12-12 13:24:02 +0300

    methods ların ayarlanması

---
+++ app.js
@@ -0,0 +1,25 @@
+new vue({
+  el: "#app",
+  data: {
+    playerHeal: 100,
+    monsterHeal: 100,
+    gameIsOn: false
+  },
+  methods: {
+    startGame: function () {
+   this.gameIsOn=true;
+    },
+    atack: function () {
+
+    },
+    specialAttac: function () {
+
+    },
+    helUp: function () {
+
+    },
+    giveUp: function () {
+
+    },
+  },
+});
